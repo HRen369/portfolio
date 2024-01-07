1591da813db56d9e17706ac5ba43e535eb90b74f
HRen369 <hrendon369@gmail.com>
2024-01-06 18:17:39 -0800

    refactored

---
+++ nav-bar/navBar.js
@@ -0,0 +1,81 @@
+const newTabLinks = [
+    {
+        "name":"Resume",
+        "link":"..\\Resume_Humberto_Rendon.pdf"
+    }
+]
+
+const links = [
+    {
+        "name":"Projects",
+        "link":"..\\projects.html"
+    },
+    {
+        "name":"Tutorials",
+        "link":"..\\tutorials.html"
+    },
+    {
+        "name":"Contact Me",
+        "link":"..\\contact.html"
+    },
+    {
+        "name":"About Me",
+        "link":"..\\aboutMe.html"
+    }
+]
+
+function navBarElement(){
+    let navBar = document.createElement('div');
+    navBar.classList.add('nav-bar')
+    
+    navBar.appendChild(navBarLeftElement());
+    navBar.appendChild(navBarRightElement());
+    return navBar;    
+}
+
+function navBarLeftElement(){
+    let navBarLeft = document.createElement('div');
+    navBarLeft.classList.add('nav-bar-left')
+
+    let navBarHomeLink = document.createElement('a');
+    navBarHomeLink.classList.add('home-link');
+    navBarHomeLink.classList.add('nav-bar-link');
+    navBarHomeLink.setAttribute('href','..\\index.html');
+
+    let navBarHomeImg = document.createElement('img');
+    navBarHomeImg.style.height = "25px";
+    navBarHomeImg.style.width = "25px";
+    navBarHomeLink.textContent = "Humberto Rendon";
+
+    navBarHomeLink.prepend(navBarHomeImg);
+    navBarLeft.appendChild(navBarHomeLink);
+    return navBarLeft;
+}
+
+function navBarRightElement(){
+    let navBarRight = document.createElement('div')
+    navBarRight.classList.add('nav-bar-right');
+
+    newTabLinks.forEach((link) => {
+        let navBarLink = document.createElement('a');
+        navBarLink.classList.add('nav-bar-link');
+        navBarLink.setAttribute('href',`${link.link}`);
+        navBarLink.textContent = `${link.name}`;
+        navBarLink.setAttribute('target','_blank');
+        navBarRight.appendChild(navBarLink);
+    })
+
+
+    links.forEach((link) => {
+        let navBarLink = document.createElement('a');
+        navBarLink.classList.add('nav-bar-link');
+        navBarLink.setAttribute('href',`${link.link}`);
+        navBarLink.textContent = `${link.name}`;
+        navBarRight.appendChild(navBarLink);
+    })
+
+    return navBarRight;
+}
+
+
+document.body.appendChild(navBarElement());
