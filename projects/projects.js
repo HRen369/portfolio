6eaaee562279abd9ed753e0543326095f31a4e91
HRen369 <hrendon369@gmail.com>
2024-01-25 12:44:18 -0800

    more refactoring

--- projects/projects.js
+++ projects/projects.js
@@ -1,62 +1,81 @@
 const htmlCssJsProjects = [
     {
         "name":"Expense Calculator",
-        "link":"https://github.com/HRen369/simple-projects/tree/main/htmlCssJs/expenseCalculator"
+        "link":"https://github.com/HRen369/simple-projects/tree/main/htmlCssJs/expenseCalculator",
+        "status": true
     },
     {
         "name":"Digital Clock",
-        "link":"https://github.com/HRen369/simple-projects/tree/main/htmlCssJs/digitalClock"
+        "link":"https://github.com/HRen369/simple-projects/tree/main/htmlCssJs/digitalClock",
+        "status": true
     },
     {
         "name":"Old To Do List",
-        "link":"https://github.com/HRen369/simple-projects/tree/main/htmlCssJs/oldToDoList"
+        "link":"https://github.com/HRen369/simple-projects/tree/main/htmlCssJs/oldToDoList",
+        "status": true
     }
 ]
 
 const pythonProjects = [
     {
         "name":"Name Generator",
-        "link":"https://github.com/HRen369/simple-projects/tree/main/python/001-name-generator"
+        "link":"https://github.com/HRen369/simple-projects/tree/main/python/001-name-generator",
+        "status": true
     },
     {
         "name":"Head Tails Generator",
-        "link":"https://github.com/HRen369/simple-projects/tree/main/python/002-head-tails"
+        "link":"https://github.com/HRen369/simple-projects/tree/main/python/002-head-tails",
+        "status": true
     },
     {
         "name":"Lower-Higher Game",
-        "link":"https://github.com/HRen369/simple-projects/tree/main/python/003-lower-higher"
+        "link":"https://github.com/HRen369/simple-projects/tree/main/python/003-lower-higher",
+        "status": true
     },
     {
         "name":"Temp Converter",
-        "link":"https://github.com/HRen369/simple-projects/tree/main/python/004-temp-converter"
+        "link":"https://github.com/HRen369/simple-projects/tree/main/python/004-temp-converter",
+        "status": true
     },
     {
         "name":"Age Calculator",
-        "link":"https://github.com/HRen369/simple-projects/tree/main/python/005-calculateAge"
+        "link":"https://github.com/HRen369/simple-projects/tree/main/python/005-calculateAge",
+        "status": false
     },
     {
-        "name":"Tic-Tac-Toe",
-        "link":"https://github.com/HRen369/simple-projects/tree/main/python/tic-tac-toe"
+        "name":"Encryptions",
+        "link":"https://github.com/HRen369/simple-projects/tree/main/python/006-encryptions",
+        "status": false
+    },
+    {
+        "name":"Fizz Buzz",
+        "link":"https://github.com/HRen369/simple-projects/tree/main/python/007-fizzbuzz",
+        "status": false
     },
     {
         "name":"Rock Paper Scissors",
-        "link":"https://github.com/HRen369/simple-projects/tree/main/python/rock-paper-scissors"
+        "link":"https://github.com/HRen369/simple-projects/tree/main/python/008-rock-paper-scissors",
+        "status": true
     },
     {
         "name":"Hangman",
-        "link":"https://github.com/HRen369/simple-projects/tree/main/python/hangman"
+        "link":"https://github.com/HRen369/simple-projects/tree/main/python/010-hangman",
+        "status": true
     },    
+    {
+        "name":"Tic-Tac-Toe",
+        "link":"https://github.com/HRen369/simple-projects/tree/main/python/tic-tac-toe",
+        "status": true
+    },
     {
         "name":"Keylogger",
-        "link":"https://github.com/HRen369/simple-projects/tree/main/python/keylogger"
-    },    
-    {
-        "name":"Encryptions",
-        "link":"https://github.com/HRen369/simple-projects/tree/main/python/encryptions"
+        "link":"https://github.com/HRen369/simple-projects/tree/main/python/keylogger",
+        "status": true
     },
     {
         "name":"Password Manager",
-        "link":"https://github.com/HRen369/simple-projects/tree/main/python/password-manager"
+        "link":"https://github.com/HRen369/simple-projects/tree/main/python/password-manager",
+        "status": true
     },
 ]
 
