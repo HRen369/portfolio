d22dc89d944465e6bbd6b2a754a6ba43ccc9c1c7
HRen369 <hrendon369@gmail.com>
2024-04-22 14:15:43 -0700

    added chocies to projects

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,3 +1,11 @@
 <template>
-    <h1>Home</h1>
+    <div class="content">
+        <img  class="self-img" height="250px" width="250px"/>
+        <div class="self-synopsis">
+            <h2>Humberto Rendon</h2>
+            <h4>Who am I</h4>
+            <p>I'm a Software Developer from California. I went to San Luis Obispo California Polytechnic University and graduated in Spring 2023. Currently, I'm looking for a job and working on personal projects. You can see these projects in my github repo or in my projects page.</p>
+            <h4>This website is currently a work in progress</h4>
+        </div>
+    </div>
 </template>
--- src/components/ProjectsView.vue
+++ src/components/ProjectsView.vue
@@ -1,7 +1,35 @@
 <script setup>
-import NavBar from "./NavBar.vue";
 </script>
 
 <template>
-    <h1>Projects</h1>
+    <h1 class="title">Projects</h1>
+    <hr/>
+    <div class="project-nav">
+        <h3>Beginner</h3>
+        <h3>Intermediate</h3>
+        <h3>Advanced</h3>
+    </div>
 </template>
+
+<style>
+    .project-nav{
+        margin: 0;
+        display: flex;
+    }
+    
+    .project-nav h3{
+        background-color: #31304D;
+        padding: 0.5em;
+        margin-left: 1em;
+        margin-top: 0.5em;
+        border-radius: 5px;
+        box-shadow: 0.5px 0.5px;
+    }
+
+    .project-nav h3:hover{
+    background-color: #100f1b;
+    cursor: pointer;
+}
+
+
+</style>
